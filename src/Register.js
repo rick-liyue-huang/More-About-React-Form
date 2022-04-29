8f5b6520e1e11281a75cb6e2cc6e75d07a9b86f3
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-04-29 10:45:01 +1000

    feat: add pwd input

--- src/Register.js
+++ src/Register.js
@@ -54,7 +54,75 @@ const Register = () => {
 
 	return (
 		<section>
-			
+			<p
+				ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}
+				aria-live={'assertive'}
+			>
+				{errMsg}
+			</p>
+			<h1>Register</h1>
+			<form>
+				<label htmlFor="username">
+					USERNAME:
+					<span className={validName ? 'valid' : 'hide'}>
+						<FontAwesomeIcon icon={faCheck} />
+					</span>
+					<span className={validName || !user ? 'hide' : 'invalid'}>
+						<FontAwesomeIcon icon={faTimes} />
+					</span>
+				</label>
+				<input
+					type="text"
+					id={'username'}
+					ref={userRef}
+					autoComplete={'off'}
+					onChange={e => setUser(e.target.value)}
+					required
+					aria-invalid={validName ? 'false' : 'true'}
+					aria-describedby={'uidnote'}
+					onFocus={() => setUserFocus(true)}
+					onBlur={() => setUserFocus(false)}
+				/>
+				<p
+					id={'uidnote'} className={userFocus && user && !validName ? 'instructions' : 'offscreen'}
+				>
+					<FontAwesomeIcon icon={faInfoCircle} />
+					4 to 24 characters <br/>
+					Must begin with a letter <br/>
+					Letters numbers underscores hyphens allowed
+				</p>
+
+				<label htmlFor="password">
+					PASSWORD:
+					<span
+						className={validPwd ? 'valid' : 'hide'}
+					>
+						<FontAwesomeIcon icon={faCheck} />
+					</span>
+					<span className={validPwd || !pwd ? 'hide' : 'invalid'}>
+						<FontAwesomeIcon icon={faTimes} />
+					</span>
+				</label>
+				<input
+					type="password"
+					id={'password'}
+					onChange={e => setPwd(e.target.value)}
+					required
+					aria-invalid={validPwd ? 'false' : 'true'}
+					aria-describedby={'pwdnote'}
+					onFocus={() => setPwdFocus(true)}
+					onBlur={() => setPwdFocus(false)}
+				/>
+				<p
+					id={'pwdnote'} className={pwdFocus && !validPwd ? 'instructions' : 'offscreen'}
+				>
+					<FontAwesomeIcon icon={faInfoCircle} />
+					8 to 24 characters <br/>
+					Must include uppercase and lowercase letters, a number and a special character <br/>
+					Allowed special characters: <span aria-label={'exclamation mark'}>!</span> <span aria-label={'at symbol'}>@</span> <span aria-label={'hashtag'}>#</span> <span aria-label={'dollar sign'}>$</span> <span aria-label={'percent'}>%</span>
+				</p>
+
+			</form>
 		</section>
 	);
 };
